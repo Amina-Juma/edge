18f84b5064eec9a1ae572a73ac3ca2482b087086
Amina-Juma <aminagidudu93@gmail.com>
2019-08-07 17:38:49 +0300

    Basic page structure

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+	$page = "home";
+?>
+<html>
+	<head>	
+		<title>
+				Edge Technologies - <?php echo $page;?>
+		</title>		
+		<body>
+			<header>
+			</header>
+			<main>
+			</main>
+			<footer>
+			</footer>
+		</body>
+	</head>
+</html>
